143406f640c8e5f9cad2367d1fc22043cd625f92
Elena Gagliano <ekg0022@auburn.edu>
2023-11-28 08:12:48 -0600

    updating CI fxn

--- FinalProj.R
+++ FinalProj.R
@@ -12,6 +12,7 @@ beta_ls <- function(beta, x, y){
     xi <- as.matrix(unlist(x[i,]), ncol = 1) #format it so dimensions work out later
     yi <- y[i]
     pi <- pf(xi,as.matrix(beta))
+   
     
     res <- (-1*yi)*(log(pi)) - (1 - yi)*log(1-pi)
     sum <- sum + res
@@ -21,13 +22,16 @@ beta_ls <- function(beta, x, y){
 }
 
 log_reg <- function(X, y){
+  B_initial <- solve(t(X)%*%X)%*%t(X)%*%y
   result <- optim(par = B_initial, fn = beta_ls, x = X, y = y)
   return(result$par)
 }
 
 
+
+
 ##generate random data 
-#set.seed(123)
+set.seed(123)
 x1 <- rnorm(100)
 x2 <- rnorm(100)
 x3 <- rnorm(100)
@@ -53,7 +57,7 @@ log_reg(X,y)
 
 bootstrap_conf_intervals <- function(X, y, alpha = 0.05, n_bootstraps = 20) {
   n <- nrow(X)
-  beta_bootstraps <- matrix(NA, ncol = n_bootstraps, nrow = length(name_log_regressionfxn(X, y)))
+  beta_bootstraps <- matrix(NA, ncol = n_bootstraps, nrow = length(log_reg(X, y)))
   
   for (i in 1:n_bootstraps) {
     # Sample with replacement
@@ -62,13 +66,34 @@ bootstrap_conf_intervals <- function(X, y, alpha = 0.05, n_bootstraps = 20) {
     y_bootstrap <- y[indices]
     
     # Run logistic regression on the bootstrap sample
-    beta_bootstraps[, i] <- namelogregressionfxn(X_bootstrap, y_bootstrap)
+    beta_bootstraps[, i] <- log_reg(X_bootstrap, y_bootstrap)
   }
   
   # Calculate confidence intervals
-  lower <- apply(beta_bootstraps, 1, function(col) quantile(col, alpha / 2))
-  upper <- apply(beta_bootstraps, 1, function(col) quantile(col, 1 - alpha / 2))
+  lower <- apply(beta_bootstraps, 1, function(row) quantile(row, alpha / 2))
+  upper <- apply(beta_bootstraps, 1, function(row) quantile(row, 1 - alpha / 2))
   
   intervals <- data.frame(lower = lower, upper = upper)
   return(intervals)
 }
+
+# test bootstrap
+set.seed(123)
+x1 <- rnorm(100)
+x2 <- rnorm(100)
+x3 <- rnorm(100)
+X <- cbind(1, x1, x2, x3) 
+beta0 <- -1
+beta1 <- 2
+beta2 <- -0.5
+beta3 <- 1.5
+true_beta <- matrix(c(beta0, beta1, beta2, beta3), ncol = 1)
+p <- 1 / (1 + exp(-(X %*% true_beta)))
+y <- rbinom(100, 1, p)
+
+# Bootstrap confidence intervals
+intervals <- bootstrap_conf_intervals(X, y)
+
+# Print the resulting intervals
+print(intervals)
+
